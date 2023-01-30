250444be38d808cc72d5cdfddb872ce6665f12ec
Evgeny <beloysov_jenya@inbox.ru>
2023-01-30 17:36:50 +0300

    add tasks

---
+++ task3/Program.cs
@@ -0,0 +1,5 @@
+﻿// Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива. [3 7 22 2 78] -> 76
+
+Random rand = new Random();
+double temp = Math.Round(rand.Next(-10, 10) + rand.NextDouble(), 3);
+System.Console.WriteLine(temp);
