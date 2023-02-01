b18958f41e50aee7937192be97ba133acc898eed
Evgeny <beloysov_jenya@inbox.ru>
2023-02-01 17:37:08 +0300

    add tasks 3

--- task3/Program.cs
+++ task3/Program.cs
@@ -1,5 +1,50 @@
 ﻿// Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива. [3 7 22 2 78] -> 76
 
-Random rand = new Random();
-double temp = Math.Round(rand.Next(-10, 10) + rand.NextDouble(), 3);
-System.Console.WriteLine(temp);
+
+void FillArrayRandomNumbers(double[] temp)
+{
+    for (int i = 0; i < temp.Length; i++)
+    {
+        temp[i] = Convert.ToDouble(new Random().Next(100, 1000)) / 100;
+    }
+}
+
+void PrintArray(double[] arr)
+{
+    System.Console.WriteLine("[" + string.Join(", ", arr) + "]");
+}
+
+void CheckNum(double[] temp)
+{
+    double min = Int32.MaxValue;
+    double max = Int32.MinValue;
+
+    for (int i = 0; i < temp.Length; i++)
+    {
+        if (temp[i] > max)
+        {
+            max = temp[i];
+        }
+        if (temp[i] < min)
+        {
+            min = temp[i];
+        }
+    }
+    Console.WriteLine($"Всего {temp.Length} чисел. Максимальное значение = {max}, минимальное значение = {min}");
+    Console.WriteLine($"Разница между максимальным и минимальным значением = {max - min}");
+}
+
+double[] FillArrayWithRandomNumbers()
+{
+Console.WriteLine("Введите размер массива:  ");
+int size = Convert.ToInt32(Console.ReadLine());
+double[] temp = new double[size];
+FillArrayRandomNumbers(temp);
+Console.WriteLine("массив: ");
+PrintArray(temp);
+return temp;
+}
+
+double[] mass = FillArrayWithRandomNumbers();
+CheckNum(mass);
+
